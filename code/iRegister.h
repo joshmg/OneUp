57da2bf99d7c6b2dfaf0abccb16a6853794abc01
Joshua Green <joshmg@gmail.com>
2011-01-12 01:35:06 -0500

    Partially composed documentation for iRegister interface class. Redesigned iRegister to be able to be modified (as originally intended).

--- code/iRegister.h
+++ code/iRegister.h
@@ -11,31 +11,84 @@ class Word;
 class Register;
 
 class iRegister {
+    /*  iRegister is a component to mimic the functionality of Registers
+        found within the Wi-11 Machine. iRegister is the base class, which
+        will be derived General Purpose registers as well as a Program
+        Counter class. Registers are expected to modify the CCR when
+        applicable. iRegister provides basic functionality, including 
+        addition, subtraction, logical and/or/not, and storing. The direct
+        value contained within iRegister is accessible.
+    */
   private:
     
   public:
     virtual Word getValue() const = 0;
+    /*  getValue() copies and returns the value stored within iRegister.
+        The value stored within iRegister, as we as the object itself
+        remain unchanged and are therefore the function call is const.
+    */
 
+    virtual void Add(const iWord&) = 0;
     virtual Register Add(const iRegister&) const = 0;
+    /*  Add(iRegister) mathematically adds the value of the given
+        parameter's value with its current value and returns the result.
+        Neither the parameter nor the current instance of iRegister is
+        changed, therefore the function call is const.
+    */
+
+    virtual void operator+(const iWord&) = 0;
     virtual Register operator+(const iRegister&) const = 0;
-    
+    /*  The addition operator contains the same functionality as Add()
+        and therefore is merely an alias.
+    */
+
+    virtual void Subtract(const iWord&) = 0;
     virtual Register Subtract(const iRegister&) const = 0;
+    /*  Subtract(iRegister) mathematically subtracts the value of the given
+        parameter's value from the current value of this iRegister and
+        returns the result. Neither the parameter nor the current instance
+        of iRegister is changed, therefore the function call is const.
+    */
+
+    virtual void operator-(const iWord&) = 0;
     virtual Register operator-(const iRegister&) const = 0;
+    /*  The subtraction operator contains the same functionality as
+        Subtract() and therefore is merely an alias.
+    */
 
+    virtual void And(const iWord&) = 0;
     virtual Register And(const iRegister&) const = 0;
+    /*  And(iRegister) logically ands the value of the given parameter's
+        value from the current value of this iRegister and returns the
+        result. Neither the parameter nor the current instance of iRegister
+        is changed, therefore the function call is const.
+    */
+
+    virtual void Or(const iWord&) = 0;
     virtual Register Or(const iRegister&) const = 0;
-    virtual Register Not(const iRegister&) const = 0;
+    /*  Or(iRegister) logically or's the value of the given parameter's value
+        from the current value of this iRegister and returns the result.
+        Neither the parameter nor the current instance of iRegister is
+        changed, therefore the function call is const.
+    */
 
+    virtual void Not() = 0;
+    virtual Register Not() const = 0;
+    /*  Not() logically inverts the value of the current value of this
+        iRegister and returns the result. The current instance of iRegister
+        is unchanged, therefore the function call is const.
+    */
+
+    virtual void Store(const iWord&) = 0;
     virtual void Store(const iRegister&) const = 0;
-    virtual void Store(const iWord&) const = 0;
 
-    virtual Register linkCCR(iRegister* const) const = 0;
+    virtual void operator=(const iWord&) = 0;
+    virtual void operator=(const iRegister&) = 0;
 
-    virtual Register operator++() const = 0;
-    virtual Register operator++(int) const = 0;
+    virtual void linkCCR(iRegister* const) const = 0;
 
-    virtual Register operator=(const iWord&) const = 0;
-    virtual Register operator=(const iRegister&) const = 0;
+    virtual Register& operator++() = 0;
+    virtual Register& operator++(int) = 0;
 };
 
 #endif
