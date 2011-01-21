db9a6717dba2bce341b86bf24561937f4026e602
Joshua Green <joshmg@gmail.com>
2011-01-21 17:38:57 -0500

    Memory is complete and functional. Fixed errors in ResultCodes.h that were preventing compiling. Edited Memory's interface, removing a const identifier that was incorrectly placed.

---
+++ code/Memory.h
@@ -0,0 +1,38 @@
+// File: Memory.h
+// Written by Joshua Green
+
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include "iMemory.h"
+#include "iWord.h"
+#include "ResultCodes.h"
+#include <map>
+#include <vector>
+
+class Word;
+
+/*class WordCompare {
+  bool operator() (const Word& a, const Word& b) const;
+};*/
+
+class Memory : iMemory {
+  private:
+    std::vector<Word*> _bounded_memory;
+    std::vector<int> _segment_offsets;
+    std::vector<int> _segment_lengths;
+
+    std::map<int, Word> _unbounded_memory;
+
+  public:
+    virtual ~Memory();
+
+    virtual Codes::RESULT Reserve(const iWord& initial_address, const iWord& length);
+
+    virtual Word Load(const iWord&) const;
+
+    virtual Codes::RESULT Store(const iWord& address, const Word& value);
+
+};
+
+#endif
