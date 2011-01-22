6209566a9fafa2c57026d1d23798c68da4e627b5
andrewcanale <andrewcanale@gmail.com>
2011-01-22 11:19:19 -0500

    Coded ResultCodes.cpp

---
+++ code/ResultCodes.cpp
@@ -0,0 +1,24 @@
+// File: ResultCodes.cpp
+// coded by Andrew Canale
+
+#include "ResultCodes.h"
+#include <string>
+
+using namespace std;
+using namespace Codes;
+
+string ResultDecoder::Find () const {
+
+	// retrieve iterator from map of codes
+	map<Result, string>::iterator it = _codes.find(Result);
+
+	// check if retrieved iterator is the same as the map::end
+	if( it == _codes.end () ) {
+		return "Error: Undefined Result Code Enumeration";
+	}
+	else {
+		return it -> second;
+	}
+
+}
+
