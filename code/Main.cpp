a04faf56834400e8f96de31c9f0cdb3c960cd2b6
andrewgroot <QuantumChaos7@gmail.com>
2011-01-21 20:32:20 -0500

    More documentation.

--- code/Main.cpp
+++ code/Main.cpp
@@ -52,48 +52,58 @@ int main(int argc, char* argv[]) {
     and a program counter (PC).  This software package is meant to emulate
     its execution, as well as present the user with information regarding the
     state of the machine after each instruction is executed.  However,
-    before one can delve into the behind-the-scenes details, an understanding
-    of the input, output, and flow of control in necessary.
+    before one can delve into the behind-the-scenes details, one must understand
+    the environment.  In particular, an understanding the object file syntax and
+    the interactions between the components used in this project is necessary.
 
-    @section input Input: Object Files
-    @subsection syntax Syntax
+    @section syntax Object Files
     @par
     The object files (ususally file_name.o) that this simulator accepts
     are ascii text files with the following structure:
-    @arg One "Header Record"
-    @arg One or more "Text Records"
-    @arg One "End Record"
+    @arg One \ref header "Header Record"
+    @arg Several \ref text "Text Records"
+    @arg One \ref end "End Record"
     
-      @subsubsection header The Header Record
+      @subsection header The Header Record
       @par
-      The Header Record is a single line that begins with a capital 'H'.\n
-      The following 6 characters are a "segment name" (anything will do).\n
-      Next, there is a 4-digit Hexadecimal value corresponding to the "load address"
-      of the program.  The instructions to follow will start at this location in memory.\n
-      Finally, a second 4-digit Hexadecimal value denotes the length of the program-load segment.
+      The Header Record is a single line that prepares the system for
+      the storing the instructions to come.
       @par
-      At a glance: There is an H, a segment name, the first location where instruction can be written,
+      <b>Components</b>
+      @arg A capital 'H'.  This designates that it is the Header Record.
+      @arg A 6 character "segment name" (anything will do).
+      @arg A 4-digit Hexadecimal value that corresponds to the "load address" of the program.
+      Instructions can be written starting at this address.
+      @arg A second 4-digit Hexadecimal value that denotes the length of the program-load segment
+      (the size of memory into which the instructions will be loaded).
+      @par
+      <b>At a glance:</b> There is an 'H', a segment name, the first location where instructions can be written,
       and the number of memory locations for instuctions.
 
-      @subsubsection text Text Records
+      @subsection text Text Records
       @par
       Following the Header Record are serveral Text Records.
-      Each Text Record Corresponds to a machine instruction
+      Each Text Record corresponds to a single machine instruction
       and, like the header record, is on a single line.
       @par
-      The Text Record begins with a capital 'T'.\n
-      Then there is a 4-digit hexadecimal value designating where the instruction will be stored.\n
-      Lastly, there is a second 4-digit Hexadecimal value, the encoding of the instruction to be stored.
+      <b>Components</b>
+      @arg A capital 'T'.  This designates that it is a Text Record.
+      @arg A 4-digit hexadecimal value -- The location in memory at which the instruction will be stored.
+      @arg A second 4-digit Hexadecimal value -- The encoding of the instruction to be stored.
       @par
-      At a glance: There is a T, the location to store the instruction, and the instruction itself. 
+      <b>At a glance:</b> There is a T, the location to store the instruction, and the instruction itself. 
 
-      @subsubsection end The End Record
+      @subsection end The End Record
       @par
       The End Record is, as the name would suggest, the last line of the line.
-      Its purpose is to denote the end of instructions to be written and to give an initial value for the PC.
+      Its purpose is to denote the end of instructions to be written and to give an initial value for the PC.\n\n
       @par
-      The End Record begins with a capital 'E'.\n
-      Next, and last, a 4-digit hexadecimal value to be put into the PC.
+      <b>Components</b>
+      @arg The End Record begins with a capital 'E'.\n
+      @arg Next, and last, a 4-digit hexadecimal value to be put into the PC.
       @par
-      At a glance: There is an E, and the location in memory from which the first instruction should be fetched.
+      <b>At a glance:</b> There is an E, and the location in memory from which the first instruction should be fetched.
+
+    @section Component Interactions
+    @image html software_interaction.png 
 */
