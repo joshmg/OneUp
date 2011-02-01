9b89250169d895a0c3f39cd4e85a9485497beb46
andrewgroot <QuantumChaos7@gmail.com>
2011-02-01 10:14:42 -0800

    Rewrote Printer.h will new design.

--- lab2/code/Printer.h
+++ lab2/code/Printer.h
@@ -10,16 +10,21 @@
 #include "iListing.h"
 #include <string>
 #include <fstream>
+#include <map>
 
 class Printer : public iPrinter {
   private:
     std::ofstream _fileStream;
+    std::map<std::string, iWord&> _labels;
+    std::map<int, iWord&> _literals;
+    iWord& _length;
 
   public:
+    Printer(std::map<std::string, iWord&>* labels, std::map<int, iWord&>* literals, iWord& file_length);
     ~Printer();
 
     bool Open(std::string filename);
-    void Print(iListing& listing);
+    void Print();
 };
 
 #endif
